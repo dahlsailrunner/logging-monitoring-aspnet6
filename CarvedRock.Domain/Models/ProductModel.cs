3d9a3d7aa18dad6f9e82000e5e35b8fdd4b1e402
Erik Dahl <erik.dahl@realpage.com>
2021-12-05 09:22:55 -0600

    updated product model for nullables

--- CarvedRock.Domain/Models/ProductModel.cs
+++ CarvedRock.Domain/Models/ProductModel.cs
@@ -3,8 +3,8 @@ namespace CarvedRock.Domain.Models;
 public class ProductModel
 { 
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
     public double Price { get; set; }
-    public string Category { get; set; }
+    public string Category { get; set; } = null!;
 }
